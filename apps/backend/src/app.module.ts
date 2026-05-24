a58e4278890f47bc0635daa242e765d1f7f7e7c9
맥북 프로16 <a2485@maegbug-ui-MacBookPro.local>
2026-05-24 17:27:31 +0900

    feat: 방 생성 구현 및 redis라이브러리를 ioredis로 통합

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,17 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { RoomModule } from './modules/room/room.module';
 import { UserModule } from './modules/user/user.module';
 import { TimerModule } from './modules/timer/timer.module';
-
 import { SentryModule, SentryGlobalFilter } from '@sentry/nestjs/setup';
 import { APP_FILTER } from '@nestjs/core';
 import { GatewayModule } from './modules/gateway/gateway.module';
 import { PrismaModule } from './common/prisma.module';
 
 import { RedisModule as CustomRedisModule } from './common/redis/redis.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
